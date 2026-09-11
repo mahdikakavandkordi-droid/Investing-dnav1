8d2ed38c7930cf3e27047460d71e04dffce1e7d6
mahdikakavandkordi-droid <mahdi.kakavandkordi@gmail.com>
2026-09-11 09:19:55 -0230

    Delete app/layout.js

--- app/layout.js
+++
@@ -1,5 +0,0 @@
-import "./globals.css";
-export const metadata = { title: "Investing DNA", description: "Discover how you invest." };
-export default function RootLayout({children}) {
-  return <html lang="en"><head><meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" /></head><body>{children}</body></html>;
-}
