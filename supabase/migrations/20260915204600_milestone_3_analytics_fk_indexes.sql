cd5d5db27120b44dccbec5933b3b8c610191b0a6
mahdikakavandkordi-droid <mahdi.kakavandkordi@gmail.com>
2026-09-15 18:32:48 -0230

    Remove superseded M3 analytics index migration timestamp

--- supabase/migrations/20260915204600_milestone_3_analytics_fk_indexes.sql
+++
@@ -1,4 +0,0 @@
-create index pilot_product_events_profile_idx on public.pilot_product_events(profile_id) where profile_id is not null;
-create index pilot_product_events_investment_idx on public.pilot_product_events(investment_id) where investment_id is not null;
-create index pilot_feedback_user_idx on public.pilot_feedback(user_id) where user_id is not null;
-create index pilot_feedback_assessment_idx on public.pilot_feedback(assessment_id) where assessment_id is not null;
